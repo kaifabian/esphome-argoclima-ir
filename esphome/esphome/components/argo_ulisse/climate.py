b0fa9448a3adb94c7d42e62b85f77b127f447d52
Kai Fabian <624090+kaifabian@users.noreply.github.com>
2026-06-17 01:14:14 +0200

    Fix crash and add support for WREM-2 protocol
    Dirty fixup to fix an early crash (using .value on an optional without
    checking whether it has been set) and frankensteining WREM-2 protocol
    support into the argo_ulisse component.
    
    Tags: #AI #generated #works-for-me

---
+++ esphome/esphome/components/argo_ulisse/climate.py
@@ -0,0 +1,28 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import climate_ir
+from esphome.const import CONF_CHANNEL
+
+AUTO_LOAD = ["climate_ir"]
+
+argo_ulisse_ns = cg.esphome_ns.namespace("argo_ulisse")
+ArgoUlisseClimate = argo_ulisse_ns.class_("ArgoUlisseClimate", climate_ir.ClimateIR)
+
+ArgoProtocolVersion = argo_ulisse_ns.enum("ArgoProtocolVersion")
+PROTOCOL_VERSIONS = {
+    "WREM-2": ArgoProtocolVersion.ARGO_PROTOCOL_WREM2,
+    "WREM-3": ArgoProtocolVersion.ARGO_PROTOCOL_WREM3,
+}
+
+CONF_PROTOCOL_VERSION = "protocol_version"
+
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(ArgoUlisseClimate).extend({
+    cv.Optional(CONF_PROTOCOL_VERSION, default="WREM-3"): cv.enum(PROTOCOL_VERSIONS, upper=True),
+    cv.Optional(CONF_CHANNEL, default=0): cv.int_range(min=0, max=3),
+})
+
+
+async def to_code(config):
+    var = await climate_ir.new_climate_ir(config)
+    cg.add(var.set_protocol_version(config[CONF_PROTOCOL_VERSION]))
+    cg.add(var.set_channel(config[CONF_CHANNEL]))
